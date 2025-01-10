d7c135599a4193e2bab22335607cf63f090d5f13
TaejungKim <tae0803@naver.com>
2025-01-10 23:12:39 +0900

    [feat/#5] LineHeightStyle 추가

--- app/src/main/java/org/memento/ui/theme/Type.kt
+++ app/src/main/java/org/memento/ui/theme/Type.kt
@@ -6,12 +6,18 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 import org.memento.R
 
 val suiteBold = FontFamily(Font(R.font.suite_bold))
 val suiteRegular = FontFamily(Font(R.font.suite_regular))
 val suiteMedium = FontFamily(Font(R.font.suite_medium))
+
+val lineHeightStyle = LineHeightStyle(
+    alignment = LineHeightStyle.Alignment.Center,
+    trim = LineHeightStyle.Trim.None
+)
 
 @Immutable
 data class MementoTypography(
@@ -51,7 +57,8 @@ val defaultMementoTypography = MementoTypography(
         fontFamily = suiteBold,
         fontSize = 40.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 50.sp
+        lineHeight = 50.sp,
+        lineHeightStyle = lineHeightStyle
     ),
 
     // title
@@ -59,25 +66,29 @@ val defaultMementoTypography = MementoTypography(
         fontFamily = suiteBold,
         fontSize = 24.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 36.sp
+        lineHeight = 36.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     title_b_22 = TextStyle(
         fontFamily = suiteBold,
         fontSize = 22.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 33.sp
+        lineHeight = 33.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     title_r_24 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 24.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 36.sp
+        lineHeight = 36.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     title_r_22 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 22.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 33.sp
+        lineHeight = 33.sp,
+        lineHeightStyle = lineHeightStyle
     ),
 
     //body
@@ -85,49 +96,57 @@ val defaultMementoTypography = MementoTypography(
         fontFamily = suiteBold,
         fontSize = 20.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 30.sp
+        lineHeight = 30.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_b_18 = TextStyle(
         fontFamily = suiteBold,
         fontSize = 18.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 27.sp
+        lineHeight = 27.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_b_16 = TextStyle(
         fontFamily = suiteBold,
         fontSize = 16.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 24.sp
+        lineHeight = 24.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_b_14 = TextStyle(
         fontFamily = suiteBold,
         fontSize = 14.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 20.sp
+        lineHeight = 20.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_r_20 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 20.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 30.sp
+        lineHeight = 30.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_r_18 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 18.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 27.sp
+        lineHeight = 27.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_r_16 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 16.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 24.sp
+        lineHeight = 24.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     body_r_14 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 14.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 20.sp
+        lineHeight = 20.sp,
+        lineHeightStyle = lineHeightStyle
     ),
 
     // detail
@@ -141,19 +160,22 @@ val defaultMementoTypography = MementoTypography(
         fontFamily = suiteRegular,
         fontSize = 12.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 17.sp
+        lineHeight = 17.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     detail_b_11 = TextStyle(
         fontFamily = suiteBold,
         fontSize = 11.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 17.sp
+        lineHeight = 17.sp,
+        lineHeightStyle = lineHeightStyle
     ),
     detail_r_11 = TextStyle(
         fontFamily = suiteRegular,
         fontSize = 11.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 17.sp
+        lineHeight = 17.sp,
+        lineHeightStyle = lineHeightStyle
     ),
 
     // caption
@@ -161,7 +183,8 @@ val defaultMementoTypography = MementoTypography(
         fontFamily = suiteMedium,
         fontSize = 9.sp,
         fontWeight = FontWeight(500),
-        lineHeight = 17.sp
+        lineHeight = 17.sp,
+        lineHeightStyle = lineHeightStyle
     )
 )
 

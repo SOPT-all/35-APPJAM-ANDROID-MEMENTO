8d5f5566bad7d25d41571a8db724144e709867fb
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-01-14 20:46:45 +0900

    [feat/#17] 각 화면 네비게이션 구현

---
+++ app/src/main/java/org/memento/presentation/onboarding/navigation/OnboardingNavigation.kt
@@ -0,0 +1,24 @@
+package org.memento.presentation.onboarding.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import org.memento.presentation.onboarding.OnboardingScreen1
+import org.memento.presentation.onboarding.OnboardingScreen2
+
+fun NavController.navigationOnboarding2(navOptions: NavOptions? = null) {
+    navigate("OnboardingScreen2", navOptions)
+}
+
+fun NavGraphBuilder.onboardingNavGraph(
+    navigateToOnboardingScreen2: () -> Unit,
+    navigateToMainScreen: () -> Unit,
+) {
+    composable("OnboardingScreen1") {
+        OnboardingScreen1(navigateToOnboardingScreen2 = navigateToOnboardingScreen2)
+    }
+    composable("OnboardingScreen2") {
+        OnboardingScreen2(navigateToMainScreen = navigateToMainScreen)
+    }
+}

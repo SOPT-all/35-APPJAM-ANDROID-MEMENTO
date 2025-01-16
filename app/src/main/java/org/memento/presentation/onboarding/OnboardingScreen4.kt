aa175b3879badf95426b89423278c3c705f803a2
hwidung <hwhwhw74@naver.com>
2025-01-16 18:55:49 +0900

    [mod/#23] 온보딩 사전 설정4 UI 구현

---
+++ app/src/main/java/org/memento/presentation/onboarding/OnboardingScreen4.kt
@@ -0,0 +1,51 @@
+package org.memento.presentation.onboarding
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.memento.R
+import org.memento.presentation.onboarding.component.OnboardingBottomButton
+import org.memento.presentation.onboarding.component.OnboardingTopAppBar
+import org.memento.presentation.onboarding.component.SocialLoginButton
+import org.memento.presentation.type.OnboardingTopType
+
+@Composable
+fun OnboardingScreen4() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        OnboardingTopAppBar(
+            type = OnboardingTopType.PAGE4,
+            onBackClick = {}
+        )
+        Spacer(Modifier.height(203.dp))
+        SocialLoginButton(
+            icon = R.drawable.img_google,
+            content = stringResource(id = R.string.onboarding_google_login),
+            onClick = {},
+        )
+        Spacer(Modifier.weight(1f))
+        OnboardingBottomButton(
+            content = R.string.onboarding_start,
+            isSelected = true,
+            onSelected = {}
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun OnboardingScreen4Preview() {
+    OnboardingScreen4()
+}
--- app/src/main/java/org/memento/presentation/onboarding/component/OnboardingTopAppBar.kt
+++ app/src/main/java/org/memento/presentation/onboarding/component/OnboardingTopAppBar.kt
@@ -1,12 +1,15 @@
 package org.memento.presentation.onboarding.component
 
+import android.widget.Space
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.memento.R
@@ -71,27 +75,50 @@ fun OnboardingTopAppBar(
                 )
             }
         }
-        Spacer(Modifier.height(10.dp))
-        OnboardingProgressBar(
-            pageNum = type.pageNum,
-            modifier = Modifier,
-        )
-        Spacer(Modifier.height(29.dp))
-        Column(
-            modifier = Modifier
-                .padding(start = 4.dp),
-        ) {
-            Text(
-                text = type.pageNum.toString(),
-                style = defaultMementoTypography.head_b_40,
-                color = darkModeColors.gray07,
+        if (type != OnboardingTopType.PAGE4) {
+            Spacer(Modifier.height(10.dp))
+            OnboardingProgressBar(
+                pageNum = type.pageNum,
+                modifier = Modifier,
             )
-            Spacer(Modifier.height(18.dp))
-            Text(
-                text = stringResource(id = type.title),
-                style = defaultMementoTypography.title_b_24,
-                color = darkModeColors.white,
-            )
+            Spacer(Modifier.height(29.dp))
+            Column(
+                modifier = Modifier
+                    .padding(start = 4.dp),
+            ) {
+                Text(
+                    text = type.pageNum.toString(),
+                    style = defaultMementoTypography.head_b_40,
+                    color = darkModeColors.gray07,
+                )
+                Spacer(Modifier.height(18.dp))
+                Text(
+                    text = stringResource(id = type.title),
+                    style = defaultMementoTypography.title_b_24,
+                    color = darkModeColors.white,
+                )
+            }
+        } else {
+            Spacer(Modifier.height(63.dp))
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.img_calendar),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .offset(y = 34.dp)
+                )
+                Text(
+                    text = stringResource(id = type.title),
+                    style = defaultMementoTypography.title_b_24,
+                    color = darkModeColors.white,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
         }
     }
 }
@@ -121,6 +148,17 @@ fun OnboardingTopAppBarPreview2() {
 fun OnboardingTopAppBarPreview3() {
     OnboardingTopAppBar(
         type = OnboardingTopType.PAGE3,
+        onBackClick = { println("Back clicked") },
+        onSkipClick = { println("Skip clicked") },
+    )
+}
+
+
+@Preview
+@Composable
+fun OnboardingTopAppBarPreview4() {
+    OnboardingTopAppBar(
+        type = OnboardingTopType.PAGE4,
         onBackClick = { println("Back clicked") },
         onSkipClick = { println("Skip clicked") },
     )

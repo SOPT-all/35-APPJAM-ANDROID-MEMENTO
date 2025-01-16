7e5f77244ec52083520d9b68b6c7ec6544a13219
hwidung <hwhwhw74@naver.com>
2025-01-16 17:00:35 +0900

    [mod/#23] 하단 버튼 컴포넌트 수정

--- app/src/main/java/org/memento/presentation/onboarding/component/OnboardingBottomButton.kt
+++ app/src/main/java/org/memento/presentation/onboarding/component/OnboardingBottomButton.kt
@@ -1,5 +1,6 @@
 package org.memento.presentation.onboarding.component
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,51 +15,53 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.memento.R
 import org.memento.presentation.util.noRippleClickable
 import org.memento.ui.theme.darkModeColors
 import org.memento.ui.theme.defaultMementoTypography
 
 @Composable
 fun OnboardingBottomButton(
-    content: String,
+    @StringRes content: Int,
     isSelected: Boolean = false,
     onSelected: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Box(
         modifier =
-            Modifier
-                .then(modifier)
-                .fillMaxWidth()
-                .height(50.dp)
-                .background(
-                    color =
-                        if (isSelected) {
-                            darkModeColors.green
-                        } else {
-                            darkModeColors.gray10
-                        },
-                    shape = RoundedCornerShape(2.dp),
-                )
-                .noRippleClickable {
-                    onSelected()
+        Modifier
+            .then(modifier)
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(
+                color =
+                if (isSelected) {
+                    darkModeColors.green
+                } else {
+                    darkModeColors.gray10
                 },
+                shape = RoundedCornerShape(2.dp),
+            )
+            .noRippleClickable {
+                onSelected()
+            },
         Alignment.Center,
     ) {
         Text(
-            text = content,
+            text = stringResource(content),
             style = defaultMementoTypography.body_b_16,
             color =
-                if (isSelected) {
-                    darkModeColors.black
-                } else {
-                    darkModeColors.gray08
-                },
+            if (isSelected) {
+                darkModeColors.black
+            } else {
+                darkModeColors.gray08
+            },
             modifier =
-                Modifier
-                    .padding(vertical = 13.dp),
+            Modifier
+                .padding(vertical = 13.dp),
         )
     }
 }
@@ -68,7 +71,7 @@ fun OnboardingBottomButton(
 fun OnboardingBottomButtonPreview() {
     val isSelected by remember { mutableStateOf(true) }
     OnboardingBottomButton(
-        content = "Next",
+        content = R.string.onboarding_next,
         isSelected = isSelected,
         onSelected = {},
         modifier = Modifier.padding(horizontal = 10.dp),

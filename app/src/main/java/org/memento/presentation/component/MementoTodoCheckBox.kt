278df70f46675c7f788a784b80cb6189a0fcde12
TaejungKim <tae0803@naver.com>
2025-01-20 21:51:38 +0900

    [refactor/#35] checkbox default padding 제거 및 조정

--- app/src/main/java/org/memento/presentation/component/MementoTodoCheckBox.kt
+++ app/src/main/java/org/memento/presentation/component/MementoTodoCheckBox.kt
@@ -2,15 +2,20 @@ package org.memento.presentation.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import org.memento.ui.theme.darkModeColors
 import org.memento.ui.theme.defaultMementoTypography
 
@@ -27,16 +32,19 @@ fun MementoTodoCheckBox(
                 .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Checkbox(
-            checked = isChecked,
-            onCheckedChange = onCheckedChange,
-            colors =
+        CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = onCheckedChange,
+                colors =
                 CheckboxDefaults.colors(
                     uncheckedColor = darkModeColors.gray05,
                     checkedColor = darkModeColors.gray05,
                     checkmarkColor = darkModeColors.black,
                 ),
-        )
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
 
         Text(
             text = content,

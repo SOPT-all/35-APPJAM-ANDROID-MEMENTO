51e628bb6d73d567307c11efb5395af334378e71
hwidung <hwhwhw74@naver.com>
2025-01-21 17:29:45 +0900

    [feat/#41] MementoAlertDialog 테스트 예시 구현

--- app/src/main/java/org/memento/presentation/component/MementoAlertDialog.kt
+++ app/src/main/java/org/memento/presentation/component/MementoAlertDialog.kt
@@ -1,13 +1,23 @@
 package org.memento.presentation.component
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import org.memento.R
 import org.memento.ui.theme.darkModeColors
 import org.memento.ui.theme.mementoColors
 
@@ -54,4 +64,31 @@ fun MementoAlertDialog(
                 .then(modifier)
                 .fillMaxWidth(),
     )
+}
+
+@Composable
+fun DeleteDialogTestScreen() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { showDialog = true }) {
+            Text(text = stringResource(id = R.string.alert_delete))
+        }
+
+        if (showDialog) {
+            MementoAlertDialog(
+                content = R.string.alert_delete,
+                confirmButtonText = R.string.alert_delete_button,
+                dismissButtonText = R.string.alert_cancel_button,
+                onConfirmButtonClick = {
+                    showDialog = false
+                },
+                onDismissButtonClick = { showDialog = false },
+            )
+        }
+    }
 }

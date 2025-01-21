7811ae5db178b459e34d636f89e278bc0ec747c9
TaejungKim <tae0803@naver.com>
2025-01-22 06:32:50 +0900

    [refactor/#56] repository 수정

--- app/src/main/java/org/memento/data/repositoryimpl/LoginRepositoryImpl.kt
+++ app/src/main/java/org/memento/data/repositoryimpl/LoginRepositoryImpl.kt
@@ -1,20 +1,22 @@
 package org.memento.data.repositoryimpl
 
 import org.memento.data.datastore.LoginDataSource
+import org.memento.data.mapper.toData.toData
+import org.memento.data.mapper.toDomain.toUserInfo
+import org.memento.data.util.handleBaseResponse
 import org.memento.domain.entity.Login
 import org.memento.domain.entity.UserInfo
 import org.memento.domain.repository.LoginRepository
 import javax.inject.Inject
 
-class LoginRepositoryImpl
-@Inject
-constructor(
+class LoginRepositoryImpl @Inject constructor(
     private val loginDataSource: LoginDataSource,
-) : LoginRepository
-{
-    override suspend fun postLogin(login: Login): Result<UserInfo>
-    =
-        runCatching {
-           // loginDataSource.postLogin(requestLoginDto = login.toData())
+) : LoginRepository {
+    override suspend fun postLogin(login: Login): Result<UserInfo> {
+        return runCatching {
+            loginDataSource.postLogin(
+                requestLoginDto = login.toData()
+            ).handleBaseResponse().getOrThrow().toUserInfo()
         }
+    }
 }

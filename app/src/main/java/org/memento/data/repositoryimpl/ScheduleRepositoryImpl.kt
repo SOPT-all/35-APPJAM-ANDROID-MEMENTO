1ae6ff6fa3e77f2c19302304e1df5874f4c13e2c
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-01-23 01:42:06 +0900

    [mod/#52] Ktlint

--- app/src/main/java/org/memento/data/repositoryimpl/ScheduleRepositoryImpl.kt
+++ app/src/main/java/org/memento/data/repositoryimpl/ScheduleRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package org.memento.data.repositoryimpl
 
+import org.memento.data.datasource.ScheduleDataSource
 import org.memento.data.mapper.toDomain.toScheduleListModel
-import org.memento.data.service.ScheduleService
 import org.memento.domain.entity.ScheduleList
 import org.memento.domain.repository.ScheduleRepository
 import javax.inject.Inject
@@ -9,11 +9,11 @@ import javax.inject.Inject
 class ScheduleRepositoryImpl
     @Inject
     constructor(
-        private val scheduleService: ScheduleService,
+        private val scheduleDataSource: ScheduleDataSource,
     ) : ScheduleRepository {
         override suspend fun getScheduleList(date: String): Result<List<ScheduleList.ScheduleWithOrderInfo>> =
             runCatching {
-                val response = scheduleService.getScheduleLists(date).data
+                val response = scheduleDataSource.getScheduleList(date).data
                 response?.toScheduleListModel() ?: throw Exception("null")
             }
     }
--- app/src/main/java/org/memento/presentation/today/navigation/TodayNavigation.kt
+++ app/src/main/java/org/memento/presentation/today/navigation/TodayNavigation.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import org.memento.presentation.navigator.route.MainNavigationBarRoute
-import org.memento.presentation.today.TodayScreen
+import org.memento.presentation.today.NewScreen
 
 fun NavController.navigationToday(navOptions: NavOptions) {
     navigate(
@@ -18,7 +18,7 @@ fun NavGraphBuilder.todayNavGraph(
     navigateToReqres: () -> Unit,
 ) {
     composable(TodayRoute.TODAY) {
-        TodayScreen()
+        NewScreen()
     }
 }
 

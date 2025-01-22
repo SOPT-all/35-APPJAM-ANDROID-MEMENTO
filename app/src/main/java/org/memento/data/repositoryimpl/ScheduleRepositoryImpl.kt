6ac4fd6583895ad7444ec6a3f6e85269221d596a
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-01-22 23:37:35 +0900

    [feat/#52] Repository 구현

---
+++ app/src/main/java/org/memento/data/repositoryimpl/ScheduleRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package org.memento.data.repositoryimpl
+
+import org.memento.data.mapper.toDomain.toScheduleListModel
+import org.memento.data.service.ScheduleService
+import org.memento.domain.entity.ScheduleList
+import org.memento.domain.repository.ScheduleRepository
+import javax.inject.Inject
+
+class ScheduleRepositoryImpl @Inject constructor(
+    private val scheduleService: ScheduleService
+) : ScheduleRepository {
+    override suspend fun getScheduleList(date: String): Result<List<ScheduleList.ScheduleWithOrderInfo>> =
+        runCatching {
+            val response = scheduleService.getScheduleLists(date).data
+            response?.toScheduleListModel() ?: throw Exception("null")
+        }
+}

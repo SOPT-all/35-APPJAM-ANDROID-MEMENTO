fdc257036e406bdfc930a4e2eeac026abfc170ed
hwidung <hwhwhw74@naver.com>
2025-01-22 17:18:44 +0900

    [chore/#47] ktlint check

--- app/src/main/java/org/memento/presentation/todo/TodoScreen.kt
+++ app/src/main/java/org/memento/presentation/todo/TodoScreen.kt
@@ -202,8 +202,8 @@ fun TodoScreen() {
 
     Column(
         modifier =
-        Modifier
-            .fillMaxSize(),
+            Modifier
+                .fillMaxSize(),
     ) {
         MementoTopBar(
             date = todoFormatDate(today),
@@ -226,8 +226,8 @@ fun TodoScreen() {
         ) {
             TodoBoxUp(
                 modifier =
-                Modifier
-                    .align(Alignment.TopCenter),
+                    Modifier
+                        .align(Alignment.TopCenter),
             )
             LazyColumn(
                 state = todolistState,
@@ -243,8 +243,8 @@ fun TodoScreen() {
                     val firstUndoneTodoId = sortedTodos.firstOrNull { !it.isCompleted }?.id
                     Column(
                         modifier =
-                        Modifier
-                            .padding(horizontal = 16.dp),
+                            Modifier
+                                .padding(horizontal = 16.dp),
                     ) {
                         sortedTodos.forEachIndexed { index, todoItem ->
                             val deadline = if (todoItem.date == todoItem.deadline) "Today" else todoFormatDate(todoItem.date.toLocalDate())
@@ -271,14 +271,14 @@ fun TodoScreen() {
             MementoAiFloatingButton(
                 onClick = {},
                 modifier =
-                Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(bottom = 20.dp, end = 20.dp),
+                    Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(bottom = 20.dp, end = 20.dp),
             )
             TodoBoxDown(
                 modifier =
-                Modifier
-                    .align(Alignment.BottomCenter),
+                    Modifier
+                        .align(Alignment.BottomCenter),
             )
         }
     }

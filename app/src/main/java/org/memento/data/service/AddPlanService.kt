5639bc9708ed6484e91563f44d3ea4daee0f4f8d
hwidung <hwhwhw74@naver.com>
2025-01-24 04:52:22 +0900

    [feat/#77] 태그 GET 서비스

--- app/src/main/java/org/memento/data/service/AddPlanService.kt
+++ app/src/main/java/org/memento/data/service/AddPlanService.kt
@@ -3,7 +3,9 @@ package org.memento.data.service
 import org.memento.data.dto.BaseResponse
 import org.memento.data.dto.request.RequestAddScheduleDto
 import org.memento.data.dto.request.RequestAddTodoDto
+import org.memento.data.dto.response.ResponseTagDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AddPlanService {
@@ -16,4 +18,7 @@ interface AddPlanService {
     suspend fun postAddSchedule(
         @Body requestAddScheduleDto: RequestAddScheduleDto,
     ): BaseResponse<Unit>
+
+    @GET("/api/v1/tags")
+    suspend fun getTagList(): BaseResponse<List<ResponseTagDto>>
 }

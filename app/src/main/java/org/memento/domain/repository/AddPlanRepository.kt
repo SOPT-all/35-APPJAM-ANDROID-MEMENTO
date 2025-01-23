5bfb1318f347f14d099844612b8eaf159caa5b92
hwidung <hwhwhw74@naver.com>
2025-01-23 22:29:43 +0900

    [feat/#74] 브레인덤프 레포지토리

--- app/src/main/java/org/memento/domain/repository/AddPlanRepository.kt
+++ app/src/main/java/org/memento/domain/repository/AddPlanRepository.kt
@@ -2,9 +2,12 @@ package org.memento.domain.repository
 
 import org.memento.domain.entity.AddSchedule
 import org.memento.domain.entity.AddTodo
+import org.memento.domain.entity.BrainDump
 
 interface AddPlanRepository {
     suspend fun postAddTodo(addTodo: AddTodo): Result<Unit>
 
     suspend fun postAddSchedule(addSchedule: AddSchedule): Result<Unit>
+
+    suspend fun postBrainDump(brainDump: BrainDump): Result<Unit>
 }

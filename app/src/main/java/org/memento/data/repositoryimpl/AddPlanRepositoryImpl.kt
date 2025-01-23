408351a2f4e35486bba05452c2fea0311633fa5d
hwidung <hwhwhw74@naver.com>
2025-01-24 04:53:17 +0900

    [feat/#77] 태그 GET 레포지토리 구현

--- app/src/main/java/org/memento/data/repositoryimpl/AddPlanRepositoryImpl.kt
+++ app/src/main/java/org/memento/data/repositoryimpl/AddPlanRepositoryImpl.kt
@@ -2,9 +2,11 @@ package org.memento.data.repositoryimpl
 
 import org.memento.data.datasource.AddPlanDataSource
 import org.memento.data.mapper.toData.toData
+import org.memento.data.mapper.toDomain.toTag
 import org.memento.data.util.handleBaseResponse
 import org.memento.domain.entity.AddSchedule
 import org.memento.domain.entity.AddTodo
+import org.memento.domain.entity.Tag
 import org.memento.domain.repository.AddPlanRepository
 import javax.inject.Inject
 
@@ -26,6 +28,13 @@ class AddPlanRepositoryImpl
                 addPlanDataSource.postAddSchedule(
                     requestAddScheduleDto = addSchedule.toData(),
                 ).handleBaseResponse().getOrThrow()
+            }
+        }
+
+        override suspend fun getTagList(): Result<List<Tag>> {
+            return runCatching {
+                val response = addPlanDataSource.getTagList()
+                response.data?.map { it.toTag() } ?: emptyList()
             }
         }
     }

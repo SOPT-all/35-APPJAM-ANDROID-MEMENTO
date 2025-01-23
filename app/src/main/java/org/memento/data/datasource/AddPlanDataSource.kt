f86d6d1e705a161dd2bbbe091845bd844c13daa0
hwidung <hwhwhw74@naver.com>
2025-01-24 04:53:07 +0900

    [feat/#77] 태그 GET 데이터소스, 구현

--- app/src/main/java/org/memento/data/datasource/AddPlanDataSource.kt
+++ app/src/main/java/org/memento/data/datasource/AddPlanDataSource.kt
@@ -3,9 +3,12 @@ package org.memento.data.datasource
 import org.memento.data.dto.BaseResponse
 import org.memento.data.dto.request.RequestAddScheduleDto
 import org.memento.data.dto.request.RequestAddTodoDto
+import org.memento.data.dto.response.ResponseTagDto
 
 interface AddPlanDataSource {
     suspend fun postAddTodo(requestAddTodoDto: RequestAddTodoDto): BaseResponse<Unit>
 
     suspend fun postAddSchedule(requestAddScheduleDto: RequestAddScheduleDto): BaseResponse<Unit>
+
+    suspend fun getTagList(): BaseResponse<List<ResponseTagDto>>
 }
--- app/src/main/java/org/memento/data/datasourceimpl/AddPlanDataSourceImpl.kt
+++ app/src/main/java/org/memento/data/datasourceimpl/AddPlanDataSourceImpl.kt
@@ -4,6 +4,7 @@ import org.memento.data.datasource.AddPlanDataSource
 import org.memento.data.dto.BaseResponse
 import org.memento.data.dto.request.RequestAddScheduleDto
 import org.memento.data.dto.request.RequestAddTodoDto
+import org.memento.data.dto.response.ResponseTagDto
 import org.memento.data.service.AddPlanService
 import javax.inject.Inject
 
@@ -17,4 +18,7 @@ class AddPlanDataSourceImpl
 
         override suspend fun postAddSchedule(requestAddScheduleDto: RequestAddScheduleDto): BaseResponse<Unit> =
             addPlanService.postAddSchedule(requestAddScheduleDto)
+
+        override suspend fun getTagList(): BaseResponse<List<ResponseTagDto>> =
+            addPlanService.getTagList()
     }

f9e6aae139709c0cca46668c17c58d4c879de362
hwidung <hwhwhw74@naver.com>
2025-01-24 04:52:43 +0900

    [feat/#77] 태그 GET 레포지토리

--- app/src/main/java/org/memento/domain/repository/AddPlanRepository.kt
+++ app/src/main/java/org/memento/domain/repository/AddPlanRepository.kt
@@ -2,9 +2,12 @@ package org.memento.domain.repository
 
 import org.memento.domain.entity.AddSchedule
 import org.memento.domain.entity.AddTodo
+import org.memento.domain.entity.Tag
 
 interface AddPlanRepository {
     suspend fun postAddTodo(addTodo: AddTodo): Result<Unit>
 
     suspend fun postAddSchedule(addSchedule: AddSchedule): Result<Unit>
+
+    suspend fun getTagList(): Result<List<Tag>>
 }

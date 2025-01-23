bca98974614319490bc46e0324e958a43df8c7d1
hwidung <hwhwhw74@naver.com>
2025-01-23 23:25:27 +0900

    [feat/#59] Mapper 정의/수정

--- app/src/main/java/org/memento/data/mapper/toData/RequestUserInfoMapper.kt
+++ app/src/main/java/org/memento/data/mapper/toData/RequestUserInfoMapper.kt
@@ -3,13 +3,14 @@ package org.memento.data.mapper.toData
 import org.memento.data.dto.request.RequestUserInfoUpdateDto
 import org.memento.domain.entity.UserInfo
 
-fun UserInfo.toData(): RequestUserInfoUpdateDto = RequestUserInfoUpdateDto(
-    wakeUpTime = wakeUpTime,
-    windDownTime = windDownTime,
-    job = job,
-    jobOtherDetail = jobOtherDetail,
-    isStressedUnorganizedSchedule = isStressedUnorganizedSchedule,
-    isForgetImportantThings = isForgetImportantThings,
-    isPreferReminder = isPreferReminder,
-    isImportantBreaks = isImportantBreaks,
-)
+fun UserInfo.toData(): RequestUserInfoUpdateDto =
+    RequestUserInfoUpdateDto(
+        wakeUpTime = wakeUpTime,
+        windDownTime = windDownTime,
+        job = job,
+        jobOtherDetail = jobOtherDetail,
+        isStressedUnorganizedSchedule = isStressedUnorganizedSchedule,
+        isForgetImportantThings = isForgetImportantThings,
+        isPreferReminder = isPreferReminder,
+        isImportantBreaks = isImportantBreaks,
+    )

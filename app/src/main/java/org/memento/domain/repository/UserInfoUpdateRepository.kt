f662896f5d6245c6eb75ac121faf6862d80ab25e
hwidung <hwhwhw74@naver.com>
2025-01-23 23:24:23 +0900

    [feat/#59] Respository 정의

--- app/src/main/java/org/memento/domain/repository/UserInfoUpdateRepository.kt
+++ app/src/main/java/org/memento/domain/repository/UserInfoUpdateRepository.kt
@@ -1,11 +1,9 @@
 package org.memento.domain.repository
 
-import org.memento.data.dto.BaseResponse
-import org.memento.data.dto.request.RequestUserInfoUpdateDto
 import org.memento.domain.entity.UserInfo
 
 interface UserInfoUpdateRepository {
     suspend fun fetchUserInfo(
-        userInfo: UserInfo
+        userInfo: UserInfo,
     ): Result<Unit>
 }

5a28115993b2739b4f879515ffee37458f443c25
hwidung <hwhwhw74@naver.com>
2025-01-24 02:29:03 +0900

    [chore/#74] ktlint check

--- app/src/main/java/org/memento/data/service/AddPlanService.kt
+++ app/src/main/java/org/memento/data/service/AddPlanService.kt
@@ -3,9 +3,9 @@ package org.memento.data.service
 import org.memento.data.dto.BaseResponse
 import org.memento.data.dto.request.RequestAddScheduleDto
 import org.memento.data.dto.request.RequestAddTodoDto
+import org.memento.data.dto.request.RequestBrainDumpDto
 import org.memento.data.dto.response.ResponseScheduleDetailDto
 import org.memento.data.dto.response.ResponseTodoDetailDto
-import org.memento.data.dto.request.RequestBrainDumpDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH

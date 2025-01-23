e38f11e034c0c8a04c44a2a946eb4b21b6c5173a
hwidung <hwhwhw74@naver.com>
2025-01-23 23:25:43 +0900

    [feat/#59] Repository 구현

--- app/src/main/java/org/memento/data/repositoryimpl/UserInfoUpdateRepositoryImpl.kt
+++ app/src/main/java/org/memento/data/repositoryimpl/UserInfoUpdateRepositoryImpl.kt
@@ -1,22 +1,21 @@
 package org.memento.data.repositoryimpl
 
 import org.memento.data.datasource.UserInfoDataSource
-import org.memento.data.dto.request.RequestUserInfoUpdateDto
 import org.memento.data.mapper.toData.toData
-import org.memento.data.service.UserInfoUpdateService
-import org.memento.data.util.handleBaseResponse
 import org.memento.domain.entity.UserInfo
 import org.memento.domain.repository.UserInfoUpdateRepository
 import javax.inject.Inject
 
-class UserInfoUpdateRepositoryImpl @Inject constructor(
-    private val userInfoDataSource: UserInfoDataSource
-) : UserInfoUpdateRepository {
-    override suspend fun fetchUserInfo(userInfo: UserInfo): Result<Unit> {
-        return runCatching {
-            userInfoDataSource.fetchUserInfo(
-                requestUserInfo = userInfo.toData()
-            )
+class UserInfoUpdateRepositoryImpl
+    @Inject
+    constructor(
+        private val userInfoDataSource: UserInfoDataSource,
+    ) : UserInfoUpdateRepository {
+        override suspend fun fetchUserInfo(userInfo: UserInfo): Result<Unit> {
+            return runCatching {
+                userInfoDataSource.fetchUserInfo(
+                    requestUserInfo = userInfo.toData(),
+                )
+            }
         }
     }
-}

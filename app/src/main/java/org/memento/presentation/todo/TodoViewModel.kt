754f50ca9b77d09faac496fe18f5a50e3af263ab
TaejungKim <tae0803@naver.com>
2025-01-24 22:46:45 +0900

    [hotifx/#97] delete api 수정

--- app/src/main/java/org/memento/presentation/todo/TodoViewModel.kt
+++ app/src/main/java/org/memento/presentation/todo/TodoViewModel.kt
@@ -19,145 +19,162 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TodoViewModel
-    @Inject
-    constructor(
-        private val todoRepository: TodoRepository,
-        private val scheduleRepository: ScheduleRepository,
-    ) : ViewModel() {
-        private val _todoItems = MutableStateFlow<List<MementoItem.TodoItem>>(emptyList())
-        val todoItems: StateFlow<List<MementoItem.TodoItem>> = _todoItems
+@Inject
+constructor(
+    private val todoRepository: TodoRepository,
+    private val scheduleRepository: ScheduleRepository,
+) : ViewModel() {
+    private val _todoItems = MutableStateFlow<List<MementoItem.TodoItem>>(emptyList())
+    val todoItems: StateFlow<List<MementoItem.TodoItem>> = _todoItems
 
-        private val _deleteState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
-        val deleteState: StateFlow<UiState<Unit>> = _deleteState
+    private val _deleteState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
+    val deleteState: StateFlow<UiState<Unit>> = _deleteState
 
-      private val _selectedDate = MutableStateFlow<LocalDate>(LocalDate.now())
-        val selectedDate: StateFlow<LocalDate> = _selectedDate
+    private val _selectedDate = MutableStateFlow<LocalDate>(LocalDate.now())
+    val selectedDate: StateFlow<LocalDate> = _selectedDate
 
-        private val _uiState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
-        val uiState: StateFlow<UiState<Unit>> = _uiState
+    private val _uiState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
+    val uiState: StateFlow<UiState<Unit>> = _uiState
 
-        private val _uiAIState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
-        val uiAIState: StateFlow<UiState<Unit>> = _uiAIState
+    private val _uiAIState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
+    val uiAIState: StateFlow<UiState<Unit>> = _uiAIState
 
-        init {
-            getTodoList()
-        }
+    init {
+        getTodoList()
+    }
 
-        fun deletePlan(
-            planId: Int,
-            dialogType: DialogType,
-        ) {
-            when (dialogType) {
-                DialogType.SCHEDULE -> {
-                    viewModelScope.launch {
-                        _deleteState.value = UiState.Loading
-                        val result = scheduleRepository.deleteSchedule(scheduleId = planId)
+    fun deletePlan(
+        planId: Int,
+        dialogType: DialogType,
+    ) {
+        when (dialogType) {
+            DialogType.SCHEDULE -> {
+                viewModelScope.launch {
+                    _deleteState.value = UiState.Loading
+                    val result = scheduleRepository.deleteSchedule(scheduleId = planId)
 
-                        _deleteState.value =
-                            result.fold(
-                                onSuccess = { UiState.Success(Unit) },
-                                onFailure = { throwable ->
-                                    Timber.e(throwable, "Failed to delete schedule")
-                                    UiState.Failure
-                                },
-                            )
-                    }
-                }
-
-                else -> {
-        fun postPriorityTodo() {
-            Timber.e("TodoViewModel", _selectedDate.value.toString())
-            viewModelScope.launch {
-                val response =
-                    todoRepository.postPriorityTodo(
-                        TargetDate(
-                            targetDate = _selectedDate.value.toString(),
-                        ),
-                    )
-                response.onSuccess { data ->
-                    val mappedData =
-                        data.priorityTodoList.toDoGetResponses.map { response ->
-                            MementoItem.TodoItem(
-                                id = response.id,
-                                description = response.description,
-                                date = response.startDate,
-                                deadline = response.endDate,
-                                isCompleted = response.isCompleted,
-                                priorityValue = response.priorityValue,
-                                priorityType = response.priorityType,
-                                tagName = response.tagName,
-                                tagColor = response.tagColor,
-                                toDoType = response.toDoType,
-                                order = response.order,
-                                groupId = response.groupId,
-                            )
-                        }
-                    _todoItems.value = mappedData
-                    _uiAIState.value = UiState.Success(Unit)
-                }.onFailure { throwable ->
-                    Log.e("TodoViewModel", "Error fetching priority todo", throwable)
-                    _uiAIState.value = UiState.Failure
+                    _deleteState.value =
+                        result.fold(
+                            onSuccess = { UiState.Success(Unit) },
+                            onFailure = { throwable ->
+                                Timber.e(throwable, "Failed to delete schedule")
+                                UiState.Failure
+                            },
+                        )
                 }
             }
-        }
 
-        private fun getTodoList() {
-            viewModelScope.launch {
-                _uiState.value = UiState.Loading
-                val response = todoRepository.getTodoList()
-                response.onSuccess { data ->
-                    val mappedData =
-                        data.map { response ->
-                            MementoItem.TodoItem(
-                                id = response.id,
-                                groupId = response.groupId,
-                                description = response.description,
-                                date = response.startDate,
-                                deadline = response.endDate,
-                                isCompleted = response.isCompleted,
-                                priorityValue = response.priorityValue,
-                                priorityType = response.priorityType,
-                                tagName = response.tagName,
-                                tagColor = response.tagColor,
-                                toDoType = response.toDoType,
-                                order = response.order,
-                            )
-                        }
-                    _todoItems.value = mappedData
-                    _uiState.value = UiState.Success(Unit)
-                }.onFailure {
-                    _uiState.value = UiState.Failure
+            else -> {
+                viewModelScope.launch {
+                    _deleteState.value = UiState.Loading
+                    val result = todoRepository.deleteTodo(toDoId = planId)
+
+                    _deleteState.value =
+                        result.fold(
+                            onSuccess = { UiState.Success(Unit) },
+                            onFailure = { throwable ->
+                                Timber.e(throwable, "Failed to delete schedule")
+                                UiState.Failure
+                            },
+                        )
                 }
             }
-        }
-
-        fun updateTodoCompletion(
-            id: Int,
-            isCompleted: Boolean,
-        ) {
-            viewModelScope.launch {
-                _todoItems.value =
-                    _todoItems.value.map { todo ->
-                        if (todo.id == id) todo.copy(isCompleted = isCompleted) else todo
-                    }
-
-                _uiState.value = UiState.Loading
-                val result = todoRepository.patchTodoComplete(id.toInt())
-                result.onSuccess {
-                    _uiState.value = UiState.Success(Unit)
-                }.onFailure {
-                    _todoItems.value =
-                        _todoItems.value.map { todo ->
-                            if (todo.id == id) todo.copy(isCompleted = !isCompleted) else todo
-                        }
-                    _uiState.value = UiState.Failure
-                }
-            }
-        }
-
-        fun updateSelectedDate(
-            selectedDate: LocalDate,
-        ) {
-            _selectedDate.value = selectedDate
         }
     }
+
+    fun postPriorityTodo() {
+        Timber.e("TodoViewModel", _selectedDate.value.toString())
+        viewModelScope.launch {
+            val response =
+                todoRepository.postPriorityTodo(
+                    TargetDate(
+                        targetDate = _selectedDate.value.toString(),
+                    ),
+                )
+            response.onSuccess { data ->
+                val mappedData =
+                    data.priorityTodoList.toDoGetResponses.map { response ->
+                        MementoItem.TodoItem(
+                            id = response.id,
+                            description = response.description,
+                            date = response.startDate,
+                            deadline = response.endDate,
+                            isCompleted = response.isCompleted,
+                            priorityValue = response.priorityValue,
+                            priorityType = response.priorityType,
+                            tagName = response.tagName,
+                            tagColor = response.tagColor,
+                            toDoType = response.toDoType,
+                            order = response.order,
+                            groupId = response.groupId,
+                        )
+                    }
+                _todoItems.value = mappedData
+                _uiAIState.value = UiState.Success(Unit)
+            }.onFailure { throwable ->
+                Log.e("TodoViewModel", "Error fetching priority todo", throwable)
+                _uiAIState.value = UiState.Failure
+            }
+        }
+    }
+
+    private fun getTodoList() {
+        viewModelScope.launch {
+            _uiState.value = UiState.Loading
+            val response = todoRepository.getTodoList()
+            response.onSuccess { data ->
+                val mappedData =
+                    data.map { response ->
+                        MementoItem.TodoItem(
+                            id = response.id,
+                            groupId = response.groupId,
+                            description = response.description,
+                            date = response.startDate,
+                            deadline = response.endDate,
+                            isCompleted = response.isCompleted,
+                            priorityValue = response.priorityValue,
+                            priorityType = response.priorityType,
+                            tagName = response.tagName,
+                            tagColor = response.tagColor,
+                            toDoType = response.toDoType,
+                            order = response.order,
+                        )
+                    }
+                _todoItems.value = mappedData
+                _uiState.value = UiState.Success(Unit)
+            }.onFailure {
+                _uiState.value = UiState.Failure
+            }
+        }
+    }
+
+    fun updateTodoCompletion(
+        id: Int,
+        isCompleted: Boolean,
+    ) {
+        viewModelScope.launch {
+            _todoItems.value =
+                _todoItems.value.map { todo ->
+                    if (todo.id == id) todo.copy(isCompleted = isCompleted) else todo
+                }
+
+            _uiState.value = UiState.Loading
+            val result = todoRepository.patchTodoComplete(id.toInt())
+            result.onSuccess {
+                _uiState.value = UiState.Success(Unit)
+            }.onFailure {
+                _todoItems.value =
+                    _todoItems.value.map { todo ->
+                        if (todo.id == id) todo.copy(isCompleted = !isCompleted) else todo
+                    }
+                _uiState.value = UiState.Failure
+            }
+        }
+    }
+
+    fun updateSelectedDate(
+        selectedDate: LocalDate,
+    ) {
+        _selectedDate.value = selectedDate
+    }
+}

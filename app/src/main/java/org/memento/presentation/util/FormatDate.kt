b071fcf473a18313d13e542a3bb986c2a406aada
TaejungKim <tae0803@naver.com>
2025-01-25 03:13:09 +0900

    [hotifx/#99] edit schedule parsing 수정

--- app/src/main/java/org/memento/presentation/util/FormatDate.kt
+++ app/src/main/java/org/memento/presentation/util/FormatDate.kt
@@ -140,7 +140,7 @@ fun formatEditTime(dateString: String): String {
 }
 
 fun formatTimeTo12Hour(timeString: String): String {
-    val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.ENGLISH)
+    val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH)
     val outputFormat = SimpleDateFormat("h a", Locale.ENGLISH)
 
     return try {
@@ -150,6 +150,7 @@ fun formatTimeTo12Hour(timeString: String): String {
         timeString
     }
 }
+
 
 fun formatTextLocalDateTime(
     dateString: String,

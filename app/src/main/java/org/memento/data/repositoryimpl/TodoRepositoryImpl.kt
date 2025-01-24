402b5e6dc356059838c44af3e22928920cef2708
hwidung <hwhwhw74@naver.com>
2025-01-24 19:23:08 +0900

    [feat/#87] ai 우선순위 레포지토리 구현

--- app/src/main/java/org/memento/data/repositoryimpl/TodoRepositoryImpl.kt
+++ app/src/main/java/org/memento/data/repositoryimpl/TodoRepositoryImpl.kt
@@ -1,30 +1,41 @@
 package org.memento.data.repositoryimpl
 
 import org.memento.data.datasource.TodoDataSource
+import org.memento.data.dto.response.ResponsePriorityTodoDto
+import org.memento.data.mapper.toData.toData
+import org.memento.data.mapper.toDomain.toDomain
 import org.memento.data.mapper.toDomain.toTodoListModel
+import org.memento.data.util.handleBaseResponse
+import org.memento.domain.entity.PriorityTodoList
+import org.memento.domain.entity.TargetDate
 import org.memento.domain.entity.TodoList
 import org.memento.domain.repository.TodoRepository
 import javax.inject.Inject
 
 class TodoRepositoryImpl
-    @Inject
-    constructor(
-        private val todoDataSource: TodoDataSource,
-    ) : TodoRepository {
-        override suspend fun getTodoList(): Result<List<TodoList.ToDoGetResponse>> =
-            runCatching {
-                val response = todoDataSource.getTodoList().data
-                response?.toTodoListModel() ?: throw Exception("null")
-            }
+@Inject
+constructor(
+    private val todoDataSource: TodoDataSource,
+) : TodoRepository {
+    override suspend fun getTodoList(): Result<List<TodoList.ToDoGetResponse>> =
+        runCatching {
+            val response = todoDataSource.getTodoList().data
+            response?.toTodoListModel() ?: throw Exception("null")
+        }
 
-        override suspend fun getTodoDateList(date: String): Result<List<TodoList.ToDoGetResponse>> =
-            runCatching {
-                val response = todoDataSource.getTodoDateList(date).data
-                response?.toTodoListModel() ?: throw Exception("null")
-            }
+    override suspend fun getTodoDateList(date: String): Result<List<TodoList.ToDoGetResponse>> =
+        runCatching {
+            val response = todoDataSource.getTodoDateList(date).data
+            response?.toTodoListModel() ?: throw Exception("null")
+        }
 
-        override suspend fun patchTodoComplete(toDoId: Int): Result<Unit> =
-            runCatching {
-                todoDataSource.patchTodoComplete(toDoId)
-            }
-    }
+    override suspend fun patchTodoComplete(toDoId: Int): Result<Unit> =
+        runCatching {
+            todoDataSource.patchTodoComplete(toDoId)
+        }
+
+    override suspend fun postPriorityTodo(targetDate: TargetDate): Result<PriorityTodoList> =
+        runCatching {
+            todoDataSource.postTodoPriority(targetDate.toData()).data?.toDomain() ?: throw Exception("null")
+        }
+}

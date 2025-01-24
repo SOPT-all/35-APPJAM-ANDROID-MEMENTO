3fc7290fbdd6840f20e6fbc282579d364187cb32
hwidung <hwhwhw74@naver.com>
2025-01-24 19:19:58 +0900

    [feat/#87] ai 우선순위 repository

--- app/src/main/java/org/memento/domain/repository/TodoRepository.kt
+++ app/src/main/java/org/memento/domain/repository/TodoRepository.kt
@@ -1,5 +1,8 @@
 package org.memento.domain.repository
 
+import org.memento.data.dto.response.ResponsePriorityTodoDto
+import org.memento.domain.entity.PriorityTodoList
+import org.memento.domain.entity.TargetDate
 import org.memento.domain.entity.TodoList
 
 interface TodoRepository {
@@ -8,4 +11,6 @@ interface TodoRepository {
     suspend fun getTodoDateList(date: String): Result<List<TodoList.ToDoGetResponse>>
 
     suspend fun patchTodoComplete(toDoId: Int): Result<Unit>
+
+    suspend fun postPriorityTodo(targetDate: TargetDate): Result<PriorityTodoList>
 }

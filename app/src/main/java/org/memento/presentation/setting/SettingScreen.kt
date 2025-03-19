81c3538e2a1cb69592e4ba622b7f702b28d8593a
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-03-20 01:00:58 +0900

    [feat/#120] setting screen 틀 작성

---
+++ app/src/main/java/org/memento/presentation/setting/SettingScreen.kt
@@ -0,0 +1,15 @@
+package org.memento.presentation.setting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SettingScreen() {
+    // TODO: 세팅 화면 UI 작성
+}
+
+@Preview
+@Composable
+fun SettingScreenPreview(){
+    SettingScreen()
+}

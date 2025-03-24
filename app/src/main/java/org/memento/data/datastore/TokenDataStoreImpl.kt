f66620c519fa02a49c5a668d8b5239ef006ebf11
hwidung <hwhwhw74@naver.com>
2025-03-25 02:35:23 +0900

    [mod/#130] Interceptor 및 토큰 관련 로그 삭제

--- app/src/main/java/org/memento/data/datastore/TokenDataStoreImpl.kt
+++ app/src/main/java/org/memento/data/datastore/TokenDataStoreImpl.kt
@@ -2,7 +2,6 @@ package org.memento.data.datastore
 
 import android.content.SharedPreferences
 import androidx.datastore.preferences.core.edit
-import timber.log.Timber
 import javax.inject.Inject
 
 class TokenDataStoreImpl
@@ -13,33 +12,27 @@ class TokenDataStoreImpl
         override var accessToken: String
             get() {
                 val token = sharedPreferences.getString(ACCESS_TOKEN, "") ?: ""
-                Timber.d("TokenDataStore: Loaded accessToken = $token")
                 return token
             }
             set(value) {
-                Timber.d("TokenDataStore: Saving accessToken = $value")
                 sharedPreferences.edit().putString(ACCESS_TOKEN, value).apply()
             }
 
         override var refreshToken: String
             get() {
                 val refreshToken = sharedPreferences.getString(REFRESH_TOKEN, "") ?: ""
-                Timber.d("TokenDataStore: Loaded RefreshToken = $refreshToken")
                 return refreshToken
             }
             set(value) {
-                Timber.d("TokenDataStore: Saving RefreshToken = $value")
                 sharedPreferences.edit().putString(REFRESH_TOKEN, value).apply()
             }
 
         override var isNewUser: Boolean
             get() {
                 val isNewUser = sharedPreferences.getBoolean(IS_NEW_USER, true)
-                Timber.d("TokenDataStore: Loaded isNewUser = $isNewUser")
                 return isNewUser
             }
             set(value) {
-                Timber.d("TokenDataStore: Saving isNewUser = $value")
                 sharedPreferences.edit().putBoolean(IS_NEW_USER, value).apply()
             }
 

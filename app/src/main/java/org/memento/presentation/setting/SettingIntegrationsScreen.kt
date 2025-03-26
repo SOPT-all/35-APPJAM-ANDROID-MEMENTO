a696dcec514ba3ce0ae8c5b9e037ac0f161b1e7a
TaejungKim <tae0803@naver.com>
2025-03-26 17:00:41 +0900

    [refactor/#132] ktlint Format

--- app/src/main/java/org/memento/presentation/setting/SettingIntegrationsScreen.kt
+++ app/src/main/java/org/memento/presentation/setting/SettingIntegrationsScreen.kt
@@ -23,69 +23,76 @@ fun SettingIntegrationsScreen() {
     val dummyData = dummyIntegrations
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = darkModeColors.black)
-            .padding(horizontal = 21.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(color = darkModeColors.black)
+                .padding(horizontal = 21.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
         Text(
             text = "여기에 Top bar 들어갑니다.",
-            style = MementoTheme.typography.body_b_16.copy(
-                color = darkModeColors.white,
-            ),
-            modifier = Modifier.fillMaxWidth()
-                .padding(top = 15.dp),
-            textAlign = TextAlign.Center
+            style =
+                MementoTheme.typography.body_b_16.copy(
+                    color = darkModeColors.white,
+                ),
+            modifier =
+                Modifier.fillMaxWidth()
+                    .padding(top = 15.dp),
+            textAlign = TextAlign.Center,
         )
 
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = darkModeColors.gray09,
-                    shape = RoundedCornerShape(size = 4.dp)
-                )
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = darkModeColors.gray09,
+                        shape = RoundedCornerShape(size = 4.dp),
+                    )
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp),
         ) {
             Text(
                 text = "Connected",
-                style = MementoTheme.typography.detail_r_12.copy(
-                    color = darkModeColors.gray05
-                )
+                style =
+                    MementoTheme.typography.detail_r_12.copy(
+                        color = darkModeColors.gray05,
+                    ),
             )
 
             SettingSocialChip(
                 icon = R.drawable.img_google,
                 content = "Google Calendar",
-                onClick = { }
+                onClick = { },
             )
         }
 
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp, horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(space = 8.dp),
         ) {
             Text(
                 text = "Add",
-                style = MementoTheme.typography.detail_r_12.copy(
-                    color = darkModeColors.gray05
-                )
+                style =
+                    MementoTheme.typography.detail_r_12.copy(
+                        color = darkModeColors.gray05,
+                    ),
             )
 
             SettingSocialChip(
                 icon = R.drawable.ic_notion,
                 content = "Notion",
-                onClick = { }
+                onClick = { },
             )
 
             SettingSocialChip(
                 icon = R.drawable.ic_notion,
                 content = "Notion",
-                onClick = { }
+                onClick = { },
             )
         }
     }
@@ -100,7 +107,7 @@ val dummyIntegrations =
     listOf(
         IntegrationDummy(name = "google", isConnected = true),
         IntegrationDummy(name = "notion", isConnected = false),
-        IntegrationDummy(name = "notion", isConnected = false)
+        IntegrationDummy(name = "notion", isConnected = false),
     )
 
 @Preview

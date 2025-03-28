e784534357d0545462b0fb40f9130283a0d0c2cf
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-03-28 23:20:22 +0900

    [mod/#134] ktlint

---
+++ app/src/main/java/org/memento/data/mapper/toDomain/ResponseSettingTagMapper.kt
@@ -0,0 +1,12 @@
+package org.memento.data.mapper.toDomain
+
+import org.memento.data.dto.response.ResponseSettingTagDto
+import org.memento.domain.entity.SettingTag
+
+
+fun ResponseSettingTagDto.toSettingTag(): SettingTag =
+    SettingTag(
+        id = id,
+        name = name,
+        colorCode = colorCode
+    )
---
+++ app/src/main/java/org/memento/domain/entity/SettingTag.kt
@@ -0,0 +1,7 @@
+package org.memento.domain.entity
+
+data class SettingTag(
+    val id: Int,
+    val name: String,
+    val colorCode: String,
+)

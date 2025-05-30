a5b72cb9e45591195ca78f622944e560fcc18f02
hwidung <hwhwhw74@naver.com>
2025-05-30 21:43:25 +0900

    [feat/#140] 화면 lottie 적용

--- app/src/main/java/org/memento/presentation/onboarding/SplashScreen.kt
+++ app/src/main/java/org/memento/presentation/onboarding/SplashScreen.kt
@@ -1,28 +1,47 @@
 package org.memento.presentation.onboarding
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieClipSpec
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieAnimatable
+import com.airbnb.lottie.compose.rememberLottieComposition
 import org.memento.R
 
 @Composable
 fun SplashScreen() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.memento_lottie))
+    val lottieAnimatable = rememberLottieAnimatable()
+
+    LaunchedEffect(composition) {
+        lottieAnimatable.animate(
+            composition = composition,
+            clipSpec = LottieClipSpec.Frame(0, 1200),
+            initialProgress = 0f
+        )
+    }
+
     Box(
         modifier =
             Modifier
                 .fillMaxSize(),
         Alignment.Center,
     ) {
-        Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.logo_memento_white),
-            contentDescription = stringResource(id = R.string.onboarding_logo),
+        LottieAnimation(
+            composition = composition,
+            progress = lottieAnimatable.progress,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier.size(215.dp)
         )
     }
 }

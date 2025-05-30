0a8a4340832a7aaa381859422e2567555aab123a
hwidung <hwhwhw74@naver.com>
2025-05-30 21:24:00 +0900

    [chore/#140] ktlint format

--- app/src/main/java/org/memento/data/service/FirebaseMessagingService.kt
+++ app/src/main/java/org/memento/data/service/FirebaseMessagingService.kt
@@ -3,7 +3,6 @@ package org.memento.data.service
 import android.R.attr.data
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import timber.log.Timber
 
 class FirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {

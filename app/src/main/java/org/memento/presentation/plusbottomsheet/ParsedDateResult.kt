95011dadba9367d50843a51d6383378bcb1f9912
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-05-31 19:37:39 +0900

    [refactor/#125] 파싱 데이터 로직 수정

--- app/src/main/java/org/memento/presentation/plusbottomsheet/ParsedDateResult.kt
+++ app/src/main/java/org/memento/presentation/plusbottomsheet/ParsedDateResult.kt
@@ -4,6 +4,6 @@ import java.time.LocalDateTime
 
 data class ParsedDateResult(
     val title: String,
-    val startDate: LocalDateTime?,
+    val startDate: LocalDateTime? = null,
     val endDate: LocalDateTime? = null,
 )

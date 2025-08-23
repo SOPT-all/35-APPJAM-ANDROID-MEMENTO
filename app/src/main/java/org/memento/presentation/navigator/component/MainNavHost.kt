01d9f0059bce3aae5a09649b189fc08df0e7a43c
hwidung <hwhwhw74@naver.com>
2025-08-24 03:44:43 +0900

    [refactor/#148] 메인 네비게이션으로 부터 Onboarding Navigation 분리

--- app/src/main/java/org/memento/presentation/navigator/component/MainNavHost.kt
+++ app/src/main/java/org/memento/presentation/navigator/component/MainNavHost.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import org.memento.presentation.navigator.MainNavigator
-import org.memento.presentation.onboarding.navigation.onboardingNavGraph
 import org.memento.presentation.reqres.navigation.reqresNavGraph
 import org.memento.presentation.setting.navigation.settingNavGraph
 import org.memento.presentation.today.navigation.todayNavGraph
@@ -29,15 +28,6 @@ fun MainNavHost(
             navController = navigator.navHostController,
             startDestination = startDestination,
         ) {
-            onboardingNavGraph(
-                navigateToOnboardingScreen1 = { navigator.navigateToOnboarding1() },
-                navigateToOnboardingScreen2 = { navigator.navigateToOnboarding2() },
-                navigateToOnboardingScreen3 = { navigator.navigateToOnboarding3() },
-                navigateToOnboardingScreen4 = { navigator.navigateToOnboarding4() },
-                navigateToMainScreen = { navigator.navigateMainNavigation(BottomNavigationType.TODAY) },
-                popBackStack = { navigator.popBackStack() },
-            )
-
             todayNavGraph(
                 padding = padding,
                 navigateToSetting = { navigator.navigateToSetting() },

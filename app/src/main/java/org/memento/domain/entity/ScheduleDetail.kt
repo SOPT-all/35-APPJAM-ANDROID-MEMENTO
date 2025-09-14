9c4b0b76060a782ea00c536a2dedf363d67bb046
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-09-14 18:30:13 +0900

    [fix/#159] 다이얼로그 스케쥴 상세 엔티티 수정

--- app/src/main/java/org/memento/domain/entity/ScheduleDetail.kt
+++ app/src/main/java/org/memento/domain/entity/ScheduleDetail.kt
@@ -7,4 +7,7 @@ data class ScheduleDetail(
     val endDate: String,
     val scheduleType: String,
     val tagId: Int,
+    // 기존 데이터 클래스 유지를 위해 초기화
+    val tagName: String = "",
+    val tagColor: String = "#FFFFFF",
 )

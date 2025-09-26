95651c535d3d9524a2ff0cbc0cbf318112e2df54
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2025-09-26 22:15:33 +0900

    [refactor/#159] 이미지 crop 수정

--- app/src/main/java/org/memento/presentation/onboarding/OnboardingScreen5.kt
+++ app/src/main/java/org/memento/presentation/onboarding/OnboardingScreen5.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.memento.R
@@ -18,6 +19,7 @@ fun OnboardingScreen5() {
             modifier = Modifier.fillMaxSize(),
             painter = painterResource(R.drawable.img_onboarding_background),
             contentDescription = null,
+            contentScale = ContentScale.Crop
         )
     }
 }
